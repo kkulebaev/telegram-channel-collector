1c93675b66a361888b0f26ecd7cba83091cbfce7
Nova <konstantinkulebaev@gmail.com>
2026-04-01 11:35:20 +0000

    chore: drop media-related columns

---
+++ prisma/migrations/20260401113400_drop_media_columns/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "TelegramPost" DROP COLUMN "caption";
+ALTER TABLE "TelegramPost" DROP COLUMN "captionEntities";
+ALTER TABLE "TelegramPost" DROP COLUMN "mediaType";
+ALTER TABLE "TelegramPost" DROP COLUMN "fileId";
